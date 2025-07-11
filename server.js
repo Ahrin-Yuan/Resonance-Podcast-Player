c6ef49a23b2ef1f2596b1bddcec1163f3fd06bf6
Ahrin <hinatairene99@gmail.com>
2025-07-12 02:34:13 +0800

    update Server.js File

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const CryptoJS = require('crypto-js');
+const fetch = require('node-fetch');
 
-const express = require('express')
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+const PORT = process.env.PORT || 8000;
 
-app.listen(3000)
+const authKey = process.env.AUTH_KEY;
+const secretKey = process.env.SECRET_KEY;
+const userAgent = process.env.USER_AGENT;
+const apiEndpoint = process.env.API_ENDPOINT;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, ()=>{
+    console.log(`Server is Running on http://localhost:${PORT}`);
+});
